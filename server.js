f9702a3aaeceda72911d8485e45948008ae3d968
Kim tw <134660009+KimaeWoo@users.noreply.github.com>
2025-03-12 13:19:47 +0900

    API getAlarm 추가

--- server.js
+++ server.js
@@ -499,6 +499,7 @@ app.get('/getAlarm', async (req, res) => {
   const start = new Date(formattedDate).setHours(0, 0, 0, 0);
   const end = new Date(formattedDate).setHours(23, 59, 59, 999);
 
+  console.log(formattedDate);
   try {
     conn = await db.getConnection();
     const results = await conn.query(query, [farm_id, start, end]);
